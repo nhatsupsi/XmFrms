b67d73a58214be1e03941e434769ed5b9a411fc5
nhatsupsi <nhatluan.nguyen@student.supsi.ch>
2018-04-04 16:24:35 +0200

    Windows: update WPF copy operation

---
+++ ClockApp.WPF/Services/ClipBoardImplementation.cs
@@ -0,0 +1,25 @@
+﻿using ClockApp.Core.Forms.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ClockApp.WPF.ClipBoardImplementation))]
+namespace ClockApp.WPF
+{
+    class ClipBoardImplementation : IClipboard
+    {
+        public string GetTextFromClipBoard()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCopy(string text)
+        {
+            Clipboard.SetDataObject(text);
+        }
+    }
+}
