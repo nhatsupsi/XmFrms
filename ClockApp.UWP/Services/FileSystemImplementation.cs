0c0739a4049fa588ae76d2574a24ca124894d069
nhatsupsi <nhatluan.nguyen@student.supsi.ch>
2018-04-06 12:46:17 +0200

    Windows: modify WPF FileSystemImplementation.cs

--- ClockApp.UWP/Services/FileSystemImplementation.cs
+++ ClockApp.UWP/Services/FileSystemImplementation.cs
@@ -7,7 +7,7 @@ namespace ClockApp.UWP
     class FileSystemImplementation : IFileSystem
     {
         static int numDataChanged = 0;
-        StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+        StorageFolder storageFolder;
         Windows.Storage.Search.QueryOptions queryOptions = new Windows.Storage.Search.QueryOptions(Windows.Storage.Search.CommonFolderQuery.DefaultQuery);
         
         public void WatchFolder()
@@ -23,6 +23,7 @@ namespace ClockApp.UWP
         }
         private async void startWatching()
         {
+            storageFolder = ApplicationData.Current.LocalFolder;
             System.Diagnostics.Debug.WriteLine("Folder Path: " + storageFolder.Path);
 
             queryOptions.FolderDepth = Windows.Storage.Search.FolderDepth.Deep;
@@ -34,7 +35,16 @@ namespace ClockApp.UWP
         }
         
         private void OnAppDataChanged(Windows.Storage.Search.IStorageQueryResultBase sender, object args)
+        //object source, FileSystemEventArgs e
         {
+            /*
+             * Unfortunately we don't get any information about the sort of change or changed file - 
+             * args doesn't provide such info, because they are always null. Also unfortunately FileTypeFilters 
+             * are not assembled with watcher. What does it mean? For our above query example, we get same 
+             * notification when .avi file is added as well as .txt file. Sad conclusion at the end
+             */
+            // args has no info about what changed. check manually
+            // maybe an solution (manually): https://stackoverflow.com/questions/15575926/identifying-file-changes-in-storagefolder
             numDataChanged++;
             String message = numDataChanged + " Data changed";
             Xamarin.Forms.MessagingCenter.Send<ClockApp.Core.Forms.App, string>((ClockApp.Core.Forms.App)Xamarin.Forms.Application.Current, "AppDataChanged", message);
--- ClockApp.WPF/Services/FileSystemImplementation.cs
+++ ClockApp.WPF/Services/FileSystemImplementation.cs
@@ -9,9 +9,16 @@ namespace ClockApp.WPF
     class FileSystemImplementation : IFileSystem
     {
         static int numDataChanged = 0;
+        FileSystemWatcher watcher;
+
+        public string GetPath()
+        {
+            return watcher.Path;
+        }
+
         public void WatchFolder()
         {
-            FileSystemWatcher watcher = new FileSystemWatcher("C:\\Users\\TOSHIBA LAP\\Desktop\\untitledfolder", "*.*");
+            watcher = new FileSystemWatcher("C:\\Users\\TOSHIBA LAP\\Desktop\\untitledfolder", "*.*");
             watcher.IncludeSubdirectories = true;
             watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
                                     | NotifyFilters.FileName | NotifyFilters.DirectoryName;
@@ -29,12 +36,16 @@ namespace ClockApp.WPF
         private void OnAppDataChanged(object source, FileSystemEventArgs e)
         {
             numDataChanged++;
-            System.Windows.MessageBox.Show("(WPF) File: " + e.FullPath + " " + e.ChangeType);
+            String message = numDataChanged + "(WPF) File: " + e.FullPath + " " + e.ChangeType;
+            //System.Windows.MessageBox.Show("(WPF) File: " + e.FullPath + " " + e.ChangeType);
+            Xamarin.Forms.MessagingCenter.Send<ClockApp.Core.Forms.App, string>((ClockApp.Core.Forms.App)Xamarin.Forms.Application.Current, "AppDataChanged", message);
         }
         private void OnAppDataRenamed(object source, RenamedEventArgs e)
         {
             numDataChanged++;
-            System.Windows.MessageBox.Show(String.Format("(WPF) File: {0} renamed to {1}", e.OldFullPath, e.FullPath));
+            String message = numDataChanged + String.Format("(WPF) File: {0} renamed to {1}", e.OldFullPath, e.FullPath);
+            //System.Windows.MessageBox.Show(String.Format("(WPF) File: {0} renamed to {1}", e.OldFullPath, e.FullPath));
+            Xamarin.Forms.MessagingCenter.Send<ClockApp.Core.Forms.App, string>((ClockApp.Core.Forms.App)Xamarin.Forms.Application.Current, "AppDataChanged", message);
         }
     }
 }
