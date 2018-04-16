950560cde2550d2ba84f7389b2c1015af9679310
nhatsupsi <nhatluan.nguyen@student.supsi.ch>
2018-04-16 19:49:35 +0200

    Windows: try to commit form new Direc

---
+++ ClockApp/Data/FileSystemWatcherEventArgs.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockApp.Core.Forms.Data
+{
+    public enum ActionType
+    {
+        Changed,
+        Created,
+        Deleted,
+        Renamed
+    }
+    public class FileSystemWatcherEventArgs : EventArgs
+    {
+        public DateTime date;
+
+        public FileSystemWatcherObject target;
+        public FileSystemWatcherObject oldTargetStat;
+        public ActionType actionType;
+
+        FileSystemWatcherEventArgs(FileSystemWatcherObject target, ActionType actionType)
+        {
+            this.date = DateTime.Now;
+            this.target = target;
+            this.actionType = actionType;
+        }
+        FileSystemWatcherEventArgs(FileSystemWatcherObject target, FileSystemWatcherObject oldTargetStat, ActionType actionType)
+        {
+            this.date = DateTime.Now;
+            this.target = target;
+            this.oldTargetStat = oldTargetStat;
+            this.actionType = actionType;
+        }
+        public static FileSystemWatcherEventArgs CreateChangedEvent(FileSystemWatcherObject target)
+        {
+            return new FileSystemWatcherEventArgs(target, ActionType.Changed);
+        }
+        public static FileSystemWatcherEventArgs CreateCreatedEvent(FileSystemWatcherObject target)
+        {
+            return new FileSystemWatcherEventArgs(target, ActionType.Created);
+        }
+        public static FileSystemWatcherEventArgs CreateDeletedEvent(FileSystemWatcherObject target)
+        {
+            return new FileSystemWatcherEventArgs(target, ActionType.Deleted);
+        }
+        public static FileSystemWatcherEventArgs CreateRenamedEvent(FileSystemWatcherObject target, FileSystemWatcherObject oldTargetStat)
+        {
+            return new FileSystemWatcherEventArgs(target, oldTargetStat, ActionType.Renamed);
+        }
+        public override string ToString()
+        {
+            if (actionType == ActionType.Renamed)
+                return String.Format("{0} {1} ({3}) is {4} to {2}", date.ToString("h:mm:ss tt"), oldTargetStat.Name, target.Name, target.Type, actionType);
+            else
+                return String.Format("{0} {1} ({2}) is {3}", date.ToString("h:mm:ss tt"), target.Name, target.Type, actionType);
+        }
+
+    }
+}
---
+++ ClockApp/Data/FileSystemWatcherObject.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockApp.Core.Forms.Data
+{
+    public enum TargetType
+    {
+        Folder,
+        File
+    }
+    public class FileSystemWatcherObject
+    {
+        String name;
+        String path;
+        TargetType type;
+
+        public String Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+        public String Path
+        {
+            get
+            {
+                return this.path;
+            }
+        }
+        public TargetType Type
+        {
+            get
+            {
+                return this.type;
+            }
+        }
+
+        public FileSystemWatcherObject(String name, String path, TargetType type)
+        {
+            this.name = name;
+            this.path = path;
+            this.type = type;
+        }
+
+    }
+}
